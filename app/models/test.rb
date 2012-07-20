631c696a406387ed2360168b529f34356d6415c5
Elisa <ebaker@pathf.com>
2012-07-20 12:14:48 -0500

    Test

---
+++ app/models/test.rb
@@ -0,0 +1,6 @@
+class Test < ActiveRecord::Base
+  attr_accessible :test
+
+  # This is a test model
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaverickMixer::Application.config.secret_token = '8f1bac29adcbc3d6f12fbed0d82873773ffaa44d8937d00e228510d1a6a2c5b1860ae743cba541b24cafaee46c58e02b673862ec59731963f6078e42fdf2af46'
